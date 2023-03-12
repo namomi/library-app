96ce023844d7dfd7cb13d474bd611e8fc3b85d0b
chihun JANG <jc@chihunui-MacBookAir.local>
2023-03-12 13:41:27 +0900

    첫 번째 커밋

---
+++ src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.java
@@ -0,0 +1,39 @@
+package com.group.libraryapp.domain.user.loanhistory;
+
+import com.group.libraryapp.domain.user.User;
+
+import javax.persistence.*;
+
+@Entity
+public class UserLoanHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id = null;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    private String bookName;
+
+    private boolean isReturn;
+
+    public UserLoanHistory(User user, String bookName) {
+        this.user = user;
+        this.bookName = bookName;
+        this.isReturn = false;
+    }
+
+    protected UserLoanHistory() {
+
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void doReturn() {
+        this.isReturn = true;
+    }
+}
